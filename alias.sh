8db441bdc29c2ca44987bfeba173a630f0e00889
David Gasperoni <mcdado@gmail.com>
2013-07-18 11:41:32 +0200

    Cleaning up the alias file. Finally merged.

--- alias.sh
+++ alias.sh
@@ -1,10 +1,10 @@
-alias topcpu="top -o cpu"		# top sorted by cpu usage
-alias topmemory="top -o rsize"	# top sorted by ram usage
-alias la="ls -lhFAG"
+alias la="ls -lhAG"             # -F
 alias lm="la -t"				# by last modified
 alias md="mkdir -p"				# make a nested dir
 alias speak='say -v "Daniel" '  
 alias parla='say -v "Paolo" '
+alias topcpu="top -o cpu"		# top sorted by cpu usage
+alias topmemory="top -o rsize"	# top sorted by ram usage
 
 alias hs='history | grep --color=auto'
 
@@ -12,10 +12,9 @@ alias networklisteners='lsof -i -P | grep LISTEN'
 
 alias brup='brew update && echo "$(tput bold)Outdated packages: $(tput sgr0)    (brew upgrade to fix)" && brew outdated'
 
-
 # youtube download
 alias yt="youtube-dl -o '%(title)s (%(uploader)s) [%(id)s].%(ext)s' $1"
-alias ytl="youtube-dl -o '%(playlist)s/%(playlist_index)s. %(title)s (%(uploader)s) [%(id)s].%(ext)s' $1"
+alias ytl="youtube-dl -o '(%(uploader)s) - %(playlist)s/%(playlist_index)s. %(title)s [%(id)s].%(ext)s' $1"
 
 alias yt1080="yt -f 37/46"
 alias  yt720="yt -f 22/45"
@@ -29,7 +28,10 @@ alias  ytl360="ytl -f 18/43/34"
 
 
 # Fix for Open With menu
-alias fix-openwith='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user; killall Finder; echo "Open With has been rebuilt, Finder will relaunch"'
+alias fix-openwith='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister \
+-kill -r -domain local -domain user; \
+killall Finder; \
+echo "Open With has been rebuilt, Finder will relaunch"'
 
 # create a list of most used commands
 # alias freq='cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30'
@@ -102,38 +104,6 @@ function getsite () {
 	"$1"
 #	--wait=5 --limit-rate=300K \
 #	--no-clobber \
-#	--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0" \
+#	--user-agent="" \
 #	--domains website.org \
 }
-
-alias la="ls -lAGh"
-alias hs='history | grep --color=auto'
-alias fix-openwith='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user; killall Finder; echo "Open With has been rebuilt, Finder will relaunch"'
-
-# youtube download
-alias yt="youtube-dl -o '%(title)s (%(uploader)s) [%(id)s].%(ext)s' $1"
-alias ytl="youtube-dl -o '%(uploader)s - %(playlist)s/%(playlist_index)s. %(title)s [%(id)s].%(ext)s' $1"
-
-alias yt1080="yt -f 37/46"
-alias  yt720="yt -f 22/45"
-alias  yt480="yt -f 44/35/18"
-alias  yt360="yt -f 18/43/34"
-
-alias ytl1080="ytl -f 37/46"
-alias  ytl720="ytl -f 22/45"
-alias  ytl480="ytl -f 44/35/18"
-alias  ytl360="ytl -f 18/43/34"
-
-function getsite () {
-    wget \
-    --recursive \
-    --page-requisites \
-    --html-extension \
-    --convert-links \
-    --restrict-file-names=windows \
-    --no-parent \
-    "$1"
-#   --no-clobber \
-#   --domains website.org \
-}
-
