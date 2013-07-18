46d909c66b7dffebcf35dc502f04c66af256feb4
David Gasperoni <mcdado@gmail.com>
2013-07-18 10:06:35 +0200

    Initial merging of different environments

--- alias.sh
+++ alias.sh
@@ -105,3 +105,35 @@ function getsite () {
 #	--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:18.0) Gecko/20100101 Firefox/18.0" \
 #	--domains website.org \
 }
+
+alias la="ls -lAGh"
+alias hs='history | grep --color=auto'
+alias fix-openwith='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user; killall Finder; echo "Open With has been rebuilt, Finder will relaunch"'
+
+# youtube download
+alias yt="youtube-dl -o '%(title)s (%(uploader)s) [%(id)s].%(ext)s' $1"
+alias ytl="youtube-dl -o '%(uploader)s - %(playlist)s/%(playlist_index)s. %(title)s [%(id)s].%(ext)s' $1"
+
+alias yt1080="yt -f 37/46"
+alias  yt720="yt -f 22/45"
+alias  yt480="yt -f 44/35/18"
+alias  yt360="yt -f 18/43/34"
+
+alias ytl1080="ytl -f 37/46"
+alias  ytl720="ytl -f 22/45"
+alias  ytl480="ytl -f 44/35/18"
+alias  ytl360="ytl -f 18/43/34"
+
+function getsite () {
+    wget \
+    --recursive \
+    --page-requisites \
+    --html-extension \
+    --convert-links \
+    --restrict-file-names=windows \
+    --no-parent \
+    "$1"
+#   --no-clobber \
+#   --domains website.org \
+}
+
