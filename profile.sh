3670cf7fc032a47caf070c005f2adf2a1fd8c0a6
David Gasperoni <mcdado@gmail.com>
2013-07-19 12:29:01 +0200

    Clean up PATH to fix homebrew's warnings.

--- profile.sh
+++ profile.sh
@@ -6,13 +6,9 @@ export PS1='\[\e[1m\]\w \$\[\e[0m\] '
 export LANG=en_US.UTF-8
 export EDITOR="mate -w"
 export GIT_EDITOR="mate --name 'Git Commit Message' -w -l 1"
-export PATH="$(brew --prefix josegonzalez/php/php54)/bin:\
-/usr/local/bin:\
-/usr/local/sbin:\
-/usr/local/mysql/bin:\
-$HOME/.rvm/bin:\
-/Applications/Mkvtoolnix.app/Contents/MacOS:\
-$PATH"
+export PATH="/usr/local/sbin:\
+$PATH:\
+/Applications/Mkvtoolnix.app/Contents/MacOS"
 
 source /usr/share/git-core/git-completion.bash
 source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
@@ -21,7 +17,5 @@ if [ -f $(brew --prefix)/etc/bash_completion ]; then
 fi
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-
-# aliases
 
 source ~/Developer/Repos/dotfiles/alias.sh
