a587a95ce1e8daaf652d2c3b2c816d8027a324f3
David Gasperoni <mcdado@gmail.com>
2013-07-25 09:14:54 +0200

    Added functions from @ttscoff

--- alias.sh
+++ alias.sh
@@ -107,3 +107,21 @@ function getsite () {
 #	--user-agent="" \
 #	--domains website.org \
 }
+
+# Quickly get image dimensions from the command line
+function imgsize() {
+	local width height
+	if [[ -f $1 ]]; then
+		height=$(sips -g pixelHeight "$1"|tail -n 1|awk '{print $2}')
+		width=$(sips -g pixelWidth "$1"|tail -n 1|awk '{print $2}')
+		echo "${width} x ${height}"
+	else
+		echo "File not found"
+	fi
+}
+
+# encode a given image file as base64 and output css background property to clipboard
+function 64enc() {
+	openssl base64 -in $1 | awk -v ext="${1#*.}" '{ str1=str1 $0 }END{ print "background:url(data:image/"ext";base64,"str1");" }'|pbcopy
+	echo "$1 encoded to clipboard"
+}
