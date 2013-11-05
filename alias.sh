8840fb8d37363a33aea6de24992ea3649b0f247b
David Gasperoni <mcdado@gmail.com>
2013-11-05 22:09:17 +0100

    New alias to sleep the screen

--- alias.sh
+++ alias.sh
@@ -10,6 +10,8 @@ alias hs='history | grep --color=auto'
 
 alias networklisteners='lsof -i -P | grep LISTEN'
 
+alias sleepscreen="osascript -e 'tell application \"System Events\" to sleep'"
+
 alias speedtest='wget -O /dev/null http://speedtest.reliableservers.com/100MBtest.bin'
 
 alias brup='brew update && echo "$(tput bold)Outdated packages: $(tput sgr0)    (brew upgrade to fix)" && brew outdated'
