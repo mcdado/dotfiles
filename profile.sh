bb1175f81f164fc9ad0e6a754b7103a98d048bb0
David Gasperoni <mcdado@gmail.com>
2013-11-07 16:27:49 +0100

    Added pip configs and comments

--- profile.sh
+++ profile.sh
@@ -1,22 +1,38 @@
-## source ~/.bashrc
-
-# variables and includes
-
-export PS1='\[\e[1m\]\w \$\[\e[0m\] '
+##
+# Environment variables
+##
 export LANG=en_US.UTF-8
 export EDITOR="mate -w"
 export GIT_EDITOR="mate --name 'Git Commit Message' -w -l 1"
-export PATH="/usr/local/bin:\
-/usr/local/sbin:\
-$PATH:\
-/Applications/Mkvtoolnix.app/Contents/MacOS"
+export PS1='\[\e[1m\]\w \$\[\e[0m\] '
+export PATH="/usr/local/bin:/usr/local/sbin:$PATH:/Applications/Mkvtoolnix.app/Contents/MacOS"
 
-
+##
+# Bash completion
+##
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   source $(brew --prefix)/etc/bash_completion
 fi
 source $(brew --prefix)/Library/Contributions/brew_bash_completion.sh
 
+##
+# Other Bash files
+##
 source ~/Developer/Repos/dotfiles/alias.sh
 
+##
+# RVM (Ruby enVironment Manager)
+##
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+##
+# PIP 
+##
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV=true
+# cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+# how do we install or upgrade a global package? We can temporarily turn off this restriction 
+function system-pip(){
+   PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
