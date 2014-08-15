3a2fbbf01f76b76ce0c06bdd399719106c86de83
David Gasperoni <mcdado@gmail.com>
2014-08-16 00:56:11 +0200

    Add fp() and fk() from: http://brettterpstra.com/2014/08/15/shell-tricks-a-time-to-kill/

--- functions.sh
+++ functions.sh
@@ -128,3 +128,30 @@ change_owner_of_files() {
         chown $user:$group $i
     done
 }
+
+fp () { #find and list processes matching a case-insensitive partial-match string
+	ps Ao pid,comm|awk '{match($0,/[^\/]+$/); print substr($0,RSTART,RLENGTH)": "$1}'|grep -i $1|grep -v grep
+}
+
+# build a menu of processes matching (case-insensitive, partial) first parameter
+# now automatically tries to use the `quit` script if process is a Mac app <http://jon.stovell.info/personal/Software.html>
+fk () {
+	local cmd OPT
+	IFS=$'\n'
+	PS3='Kill which process? (q to cancel): '
+	select OPT in $(fp $1); do
+		if [[ $OPT =~ [0-9]$ ]]; then
+			cmd=$(ps -p ${OPT##* } -o command|tail -n 1)
+			if [[ "$cmd" =~ "Contents/MacOS" ]] && [[ -f /usr/local/bin/quit ]]; then
+				echo "Quitting ${OPT%%:*}"
+				cmd=$(echo "$cmd"| sed -E 's/.*\/(.*)\.app\/.*/\1/')
+				/usr/local/bin/quit -n "$cmd"
+			else
+				echo "killing ${OPT%%:*}"
+				kill ${OPT##* }
+			fi
+		fi
+		break
+	done
+	unset IFS
+}
