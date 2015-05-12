b71c1653b15741d697187928bf8572784ee88d2a
David Gasperoni <mcdado@gmail.com>
2015-05-12 11:08:56 +0200

    Add example script for local deploying of git projects for testing
    Useful to be included in staging scripts, to be personalised with paths
    with local installations.

---
+++ .local-deployment-example.sh
@@ -0,0 +1,5 @@
+COUNTER=0
+echo "Locally deploying for $(whoami)"
+rsync -a --delete-excluded --exclude-from=.rsyncrc $(pwd) ~/Sites/site-a/modules/ && COUNTER=$((COUNTER+1))
+rsync -a --delete-excluded --exclude-from=.rsyncrc $(pwd) ~/Sites/site-b/modules/ && COUNTER=$((COUNTER+1))
+echo "Deployment complete in $COUNTER projects"
