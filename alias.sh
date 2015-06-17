d7ab1ef21078234f775121067721c4526befabcf
David Gasperoni <mcdado@gmail.com>
2015-06-18 00:10:04 +0200

    Add several git aliases and remove bash git aliases

--- alias.sh
+++ alias.sh
@@ -43,8 +43,8 @@ killall Finder; \
 echo "Open With has been rebuilt, Finder will relaunch"'
 
 # Visualise Git Log
-alias git-log-tree='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
-alias git-log="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# alias git-log-tree='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
+# alias git-log="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 # Disk usage
 alias sizes="du -sh * | sort -n"
