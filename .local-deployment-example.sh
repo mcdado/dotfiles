a0c87a27114aca80e084032f50c4f3ac038ddcf3
David Gasperoni <mcdado@gmail.com>
2015-11-10 19:09:33 +0100

    Add date to local deployment example script

--- .local-deployment-example.sh
+++ .local-deployment-example.sh
@@ -1,4 +1,5 @@
 COUNTER=0
+echo $(date)
 echo "Locally deploying for $(whoami)"
 rsync -a --delete-excluded --exclude-from=.rsyncrc $(pwd) ~/Sites/site-a/modules/ && COUNTER=$((COUNTER+1))
 rsync -a --delete-excluded --exclude-from=.rsyncrc $(pwd) ~/Sites/site-b/modules/ && COUNTER=$((COUNTER+1))
