5cc98006e784fba8b03a9c6277019da0a39e8e1a
David Gasperoni <mcdado@gmail.com>
2015-12-11 12:56:48 +0100

    Generalize location of dotfiles directory
    Before, the source of truth of dotfiles directory had to be in
    ~/Developer/dotfiles, now is always wherever the profile.sh file is,
    allowing to clone the dotfiles directory wherever, and just symlink
    profile.sh as ~/.bash_profile

--- profile.sh
+++ profile.sh
@@ -35,6 +35,13 @@ fi
 ##
 # Other Bash files
 ##
-source ~/Developer/Repos/dotfiles/alias.sh
-source ~/Developer/Repos/dotfiles/functions.sh
+DOTFILES_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$DOTFILES_SOURCE" ]; do # resolve $DOTFILES_SOURCE until the file is no longer a symlink
+  DOTFILES_DIR="$( cd -P "$( dirname "$DOTFILES_SOURCE" )" && pwd )"
+  DOTFILES_SOURCE="$(readlink "$DOTFILES_SOURCE")"
+  [[ $DOTFILES_SOURCE != /* ]] && DOTFILES_SOURCE="$DOTFILES_DIR/$DOTFILES_SOURCE" # if $DOTFILES_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DOTFILES_DIR="$( cd -P "$( dirname "$DOTFILES_SOURCE" )" && pwd )"
 
+source $DOTFILES_DIR/colors.sh
+source $DOTFILES_DIR/alias.sh
