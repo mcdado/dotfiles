fca7079d34680df7d12ea42ac8db848707c63c1c
David Gasperoni <mcdado@gmail.com>
2019-06-23 15:32:37 +0200

    Change order of PATH

--- profile.sh
+++ profile.sh
@@ -90,7 +90,7 @@ export LESS=${less_opts[*]}
 # Ruby Gems
 ##
 if which ruby >/dev/null && which gem >/dev/null; then
-  PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
+  PATH="$PATH:$(ruby -r rubygems -e 'puts Gem.user_dir')/bin"
 fi
 
 ##
